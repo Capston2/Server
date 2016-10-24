c2b33b013e63ace11ca73ac0f87e2102d8cb6e3a
KimBoWoon <haioer@naver.com>
2016-10-24 11:15:48 -0400

    xml parsing success

--- run.py
+++ run.py
@@ -9,17 +9,18 @@ def hello():
 	uri = 'http://apis.data.go.kr/9710000/BillInfoService/getBillInfoList?ServiceKey=p7UBJeNZxl1cDhlLLsZT3H0ikrzKZ7miawcXdCvHKVm%2FjpxWbvKb1UWDyJlL7oNp7CTHgLejQR0QYax17zG46Q%3D%3D&numOfRows=10&pageSize=10&pageNo=1&startPage=1'
 
 	try:
-		response = requests.get(url)
-		text = response.text
+		response = requests.get(uri)
 	except:
-		return 'Error'
+		return 'HTTP GET Error!'
 
+	text = response.text.encode('utf-8')
 	soup = BeautifulSoup(text, 'lxml')
+	items = soup.findAll('item')
 
-	titles = soup.find_all('div', 'mem_name')
-	for title in titles:
-		print(title)
-	return soup
+	for x in range(len(items)):
+		print(items[x].billname.string)
+
+	return text
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=8000, debug=True)
