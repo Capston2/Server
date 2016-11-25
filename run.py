c257541febc6f31c873dfd27847498ba9f3b5080
KimBoWoon <haioer@naver.com>
2016-11-25 10:04:46 -0500

    update

--- run.py
+++ run.py
@@ -1,12 +1,17 @@
+# -*- coding: utf-8 -*-
 from flask import Flask, render_template
 from bs4 import BeautifulSoup
+from xmljson import badgerfish as bf
+from xml.etree.ElementTree import fromstring
 import requests
+from json import dumps
+import xmltodict
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	uri = 'http://apis.data.go.kr/9710000/BillInfoService/getBillInfoList?ServiceKey=p7UBJeNZxl1cDhlLLsZT3H0ikrzKZ7miawcXdCvHKVm%2FjpxWbvKb1UWDyJlL7oNp7CTHgLejQR0QYax17zG46Q%3D%3D&numOfRows=10&pageSize=10&pageNo=1&startPage=1'
+	uri = 'http://apis.data.go.kr/9710000/BillInfoService/getBillInfoList?ServiceKey=p7UBJeNZxl1cDhlLLsZT3H0ikrzKZ7miawcXdCvHKVm%2FjpxWbvKb1UWDyJlL7oNp7CTHgLejQR0QYax17zG46Q%3D%3D&numOfRows=10&pageSize=1&pageNo=1&startPage=1'
 
 	try:
 		response = requests.get(uri)
@@ -15,12 +20,14 @@ def hello():
 
 	text = response.text.encode('utf-8')
 	soup = BeautifulSoup(text, 'lxml')
-	items = soup.findAll('item')
+	items = soup.findAll('items')
 
-	for x in range(len(items)):
-		print(items[x].billname.string)
+	print(items[0])
+	print("JSON")
+	jsonString = xmltodict.parse(items[0], encoding='utf-8')
+	print(jsonString)
 
 	return render_template('index.html', items=items)
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=8000, debug=True)
+	app.run(host='0.0.0.0', port=8001, debug=True)
