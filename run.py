115de3b475e25e9acd3ac41848a6ed745348ba10
choijaewoong <jwchoi0129@gmail.com>
2016-11-28 21:23:32 +0900

    메인 페이지 기능 수정 * 최근 접수 안건, 통과 안건 목록을 출력

--- run.py
+++ run.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template
 from bs4 import BeautifulSoup
+from xmljson import badgerfish as bf
+from xml.etree.ElementTree import fromstring
+import requests
 import json
-# from xmljson import badgerfish as bf
-# from xml.etree.ElementTree import fromstring
-# import requests
-# from json import dumps
-# import xmltodict
+import xmltodict
 
 app = Flask(__name__)
 
@@ -17,15 +16,12 @@ def getXMLData(url):
 		return 'HTTP GET Error!'
 
 	text = response.text.encode('utf-8')
-	soup = BeautifulSoup(text, 'lxml')
+	soup = BeautifulSoup(text, 'xml')
 	items = soup.findAll('items')
 
-#	print(items[0])
-#	print('JSON')
 	jsonString = xmltodict.parse(str(items[0]), encoding='utf-8')
-	print(jsonString)
 
-	return dumps(jsonString)
+	return jsonString
 
 @app.errorhandler(500)
 def internalServerError(e):
@@ -33,11 +29,17 @@ def internalServerError(e):
 
 @app.route("/")
 def index():
-	uri = 'http://apis.data.go.kr/9710000/BillInfoService/getBillInfoList?ServiceKey=p7UBJeNZxl1cDhlLLsZT3H0ikrzKZ7miawcXdCvHKVm%2FjpxWbvKb1UWDyJlL7oNp7CTHgLejQR0QYax17zG46Q%3D%3D&numOfRows=10&pageSize=1&pageNo=1&startPage=1'
+	# uri = 'http://apis.data.go.kr/9710000/BillInfoService/getBillInfoList?ServiceKey=p7UBJeNZxl1cDhlLLsZT3H0ikrzKZ7miawcXdCvHKVm%2FjpxWbvKb1UWDyJlL7oNp7CTHgLejQR0QYax17zG46Q%3D%3D&numOfRows=10&pageSize=1&pageNo=1&startPage=1'
 
-	items = getXMLData(uri)
+	# recentAcceptUri : 최근 접수 의안 목록 / recentPassUri : 최근 통과 의안 목록
+	# 서버 구축용 uri로 변경하세요
+	recentAcceptUri = 'http://apis.data.go.kr/9710000/BillInfoService/getRecentRceptList?serviceKey=81X0zTkpezfhufKJUndC9%2FRZIdYNmpQznY%2BrF%2B92eKA04rYuKZj7B6kfSIwSDvwBjPRClZRMQ2vRk19eYyp57A%3D%3D&numOfRows=10&pageSize=10&pageNo=1&startPage=1'
+	recentPassUri = 'http://apis.data.go.kr/9710000/BillInfoService/getRecentPasageList?serviceKey=81X0zTkpezfhufKJUndC9%2FRZIdYNmpQznY%2BrF%2B92eKA04rYuKZj7B6kfSIwSDvwBjPRClZRMQ2vRk19eYyp57A%3D%3D&numOfRows=10&pageSize=10&pageNo=1&startPage=1'
 
-	return render_template('index.html', items = items)
+	acceptItems = getXMLData(recentAcceptUri)
+	passItems = getXMLData(recentPassUri)
+
+	return render_template('index.html', acceptItems = acceptItems['items']['item'], passItems = passItems['items']['item'])
 
 @app.route('/top')
 def top():
@@ -70,6 +72,10 @@ def post():
 @app.route('/peoplelist')
 def peoplelist():
     return render_template('peoplelist.html')
+
+@app.route('/peopledetail')
+def peopledetail():
+    return render_template('peopledetail.html')
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=8000, debug=True)
