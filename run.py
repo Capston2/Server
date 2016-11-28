dd848b5adf812c57236ff0bd2142fe5862dc4682
KimBoWoon <haioer@naver.com>
2016-11-29 00:55:22 +0900

    peopledetail modify

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 from bs4 import BeautifulSoup
 from xmljson import badgerfish as bf
 from xml.etree.ElementTree import fromstring
+from json import dumps
 import requests
 import json
 import xmltodict
@@ -18,11 +19,12 @@ def getXMLData(url):
 
     text = response.text.encode('utf-8')
     soup = BeautifulSoup(text, 'xml')
-    items = soup.findAll('item')
+    items = soup.findAll('items')
 
-	jsonString = xmltodict.parse(str(items[0]), encoding='utf-8')
+    jsonString = xmltodict.parse(str(items[0]), encoding='utf-8')
 
-	return jsonString
+    return jsonString
+
 
 @app.errorhandler(500)
 def internalServerError(e):
@@ -31,22 +33,22 @@ def internalServerError(e):
 
 @app.route("/")
 def index():
-	# uri = 'http://apis.data.go.kr/9710000/BillInfoService/getBillInfoList?ServiceKey=p7UBJeNZxl1cDhlLLsZT3H0ikrzKZ7miawcXdCvHKVm%2FjpxWbvKb1UWDyJlL7oNp7CTHgLejQR0QYax17zG46Q%3D%3D&numOfRows=10&pageSize=1&pageNo=1&startPage=1'
+    # uri = 'http://apis.data.go.kr/9710000/BillInfoService/getBillInfoList?ServiceKey=p7UBJeNZxl1cDhlLLsZT3H0ikrzKZ7miawcXdCvHKVm%2FjpxWbvKb1UWDyJlL7oNp7CTHgLejQR0QYax17zG46Q%3D%3D&numOfRows=10&pageSize=1&pageNo=1&startPage=1'
 
-	# recentAcceptUri : 최근 접수 의안 목록 / recentPassUri : 최근 통과 의안 목록
-	# 서버 구축용 uri로 변경하세요
-	recentAcceptUri = 'http://apis.data.go.kr/9710000/BillInfoService/getRecentRceptList?serviceKey=81X0zTkpezfhufKJUndC9%2FRZIdYNmpQznY%2BrF%2B92eKA04rYuKZj7B6kfSIwSDvwBjPRClZRMQ2vRk19eYyp57A%3D%3D&numOfRows=10&pageSize=10&pageNo=1&startPage=1'
-	recentPassUri = 'http://apis.data.go.kr/9710000/BillInfoService/getRecentPasageList?serviceKey=81X0zTkpezfhufKJUndC9%2FRZIdYNmpQznY%2BrF%2B92eKA04rYuKZj7B6kfSIwSDvwBjPRClZRMQ2vRk19eYyp57A%3D%3D&numOfRows=10&pageSize=10&pageNo=1&startPage=1'
+    # recentAcceptUri : 최근 접수 의안 목록 / recentPassUri : 최근 통과 의안 목록
+    # 서버 구축용 uri로 변경하세요
+    recentAcceptUri = 'http://apis.data.go.kr/9710000/BillInfoService/getRecentRceptList?serviceKey=81X0zTkpezfhufKJUndC9%2FRZIdYNmpQznY%2BrF%2B92eKA04rYuKZj7B6kfSIwSDvwBjPRClZRMQ2vRk19eYyp57A%3D%3D&numOfRows=10&pageSize=10&pageNo=1&startPage=1'
+    recentPassUri = 'http://apis.data.go.kr/9710000/BillInfoService/getRecentPasageList?serviceKey=81X0zTkpezfhufKJUndC9%2FRZIdYNmpQznY%2BrF%2B92eKA04rYuKZj7B6kfSIwSDvwBjPRClZRMQ2vRk19eYyp57A%3D%3D&numOfRows=10&pageSize=10&pageNo=1&startPage=1'
 
-	acceptItems = getXMLData(recentAcceptUri)
-	passItems = getXMLData(recentPassUri)
+    acceptItems = getXMLData(recentAcceptUri)
+    passItems = getXMLData(recentPassUri)
 
-	return render_template('index.html', acceptItems = acceptItems['items']['item'], passItems = passItems['items']['item'])
+    return render_template('index.html', acceptItems=acceptItems['items']['item'], passItems=passItems['items']['item'])
+
 
 @app.route('/search/<string:keyword>')
 def search(keyword):
     uri = 'http://apis.data.go.kr/9710000/BillInfoService/getBillInfoList?serviceKey=p7UBJeNZxl1cDhlLLsZT3H0ikrzKZ7miawcXdCvHKVm%2FjpxWbvKb1UWDyJlL7oNp7CTHgLejQR0QYax17zG46Q%3D%3D&numOfRows=999&pageSize=999&pageNo=1&startPage=1'
-    item = None
 
     xmlData = getXMLData(uri)
 
@@ -62,21 +64,20 @@ def congressman(name):
     uri = 'http://apis.data.go.kr/9710000/NationalAssemblyInfoService/getMemberCurrStateList?serviceKey=D%2FiJQ8nHK7VOJpnJoTTt%2F234%2FUsP8ujCvSRKfw60Z%2FXk0JJfUDr5FrUWAurDfQsgV7uxMKd2Sh0u0YMnAYXs9w%3D%3D&numOfRows=999&pageSize=999&pageNo=1&startPage=1'
 
     xmlData = getXMLData(uri)
-    root = fromstring(xmlData)
-    print(root.find('item'))
+    print(xmlData['items']['item'][0]['empNm'] == name)
 
-    return xmlData
+    for x in range(999):
+        if name == xmlData['items']['item'][x]['empNm']:
+            return render_template('peopledetail.html', item=xmlData['items']['item'][x])
+            # return xmlData['items']['item'][x]
 
-    # for x in range(999):
-    #     if name == xmlData[x]['item']['empnm']:
-    #         return xmlData[x]
-    #
-    # return render_template('500.html')
+    return render_template('500.html')
 
 
 @app.route('/peopledetail')
 def peopledetail():
     return render_template('peopledetail.html')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
