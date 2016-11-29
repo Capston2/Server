0380ef14f7fe54be77f41c09fa9f72df4e1f7068
choijaewoong <jwchoi0129@gmail.com>
2016-11-30 01:50:06 +0900

    검색 결과 페이지 생성 및 페이지 레이아웃 변경

--- run.py
+++ run.py
@@ -57,10 +57,12 @@ def bill_detail(id):
     bill_detail_info = bill_detail_info_dict['response']['body']['item']['receipt']
 
     bill_petition_member_list = []
-    for k in bill_petition_member_dict['response']['body']['items']['item']:
-        if (k['gbn1'] == '의안'):
-            if ([k['memName'], k['polyNm']] not in bill_petition_member_list):
-                bill_petition_member_list.append([k['memName'], k['polyNm']])
+
+    if bill_petition_member_dict['response']['body']['items']:
+        for k in bill_petition_member_dict['response']['body']['items']['item']:
+            if (k['gbn1'] == '의안'):
+                if ([k['memName'], k['polyNm']] not in bill_petition_member_list):
+                    bill_petition_member_list.append([k['memName'], k['polyNm']])
 
     bill_summary = bill_summary_crawler('PRC_R1M6R1J1R2K9Y1B8A0J4Z2K1L3M0C0').split('\n')
 
@@ -85,6 +87,31 @@ def get_congressman_list(page):
 
     return render_template('peoplelist.html', curPage=page, numOfPages=numOfPages, congressmanList=congressmanList)
 
+@app.route('/congressman/detail/<name>')
+def get_congressman_detail(name):
+    congressman_list_url = 'http://apis.data.go.kr/9710000/NationalAssemblyInfoService/getMemberCurrStateList?ServiceKey=%s&numOfRows=999&pageNo=1' % (server_key)
+    congressman_list_xml = requests.get(congressman_list_url).content
+    congressman_list_dict = xmltodict.parse(congressman_list_xml)
+
+    if congressman_list_dict['response']['header']['resultCode'] != '00':
+        return render_template('500.html'), 500
+
+    congressman_list = []
+    for k in congressman_list_dict['response']['body']['items']['item']:
+        if k['empNm'] == name:
+            congressman_list.append(k)
+
+    congressman_detail_info_url = 'http://apis.data.go.kr/9710000/NationalAssemblyInfoService/getMemberDetailInfoList?serviceKey=%s&numOfRows=1&pageNo=1&dept_cd=%s&num=%s' % (server_key, congressman_list[0]['deptCd'], congressman_list[0]['num'])
+    congressman_detail_info_xml = requests.get(congressman_detail_info_url).content
+    congressman_detail_info_dict = xmltodict.parse(congressman_detail_info_xml)
+
+    if congressman_detail_info_dict['response']['header']['resultCode'] != '00':
+        return render_template('500.html'), 500
+
+    congressman_detail_info = congressman_detail_info_dict['response']['body']['item']
+
+    return render_template('peopledetail.html', congressmanPicture=congressman_list[0]['jpgLink'], congressmanDetailInfo=congressman_detail_info)
+
 
 def bill_summary_crawler(bill_id):
     url = 'http://likms.assembly.go.kr/bill/billDetail.do?billId='+str(bill_id)
@@ -93,6 +120,10 @@ def bill_summary_crawler(bill_id):
     soup = BeautifulSoup(plain_text, 'lxml')
 
     return soup.find(id='summaryContentDiv').text
+
+@app.route('/search')
+def get_search_result() :
+    return render_template('searchresult.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
